a080901f3a7b26fadf287c2b2c794460252133c2
Allen Fang <allenfang08@gmail.com>
2017-06-29 13:52:49 -0700

    added validation function

--- src/components/PostsNew.js
+++ src/components/PostsNew.js
@@ -2,18 +2,59 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
+	renderField(field) {
+		return (
+			<div className="form-group">
+				<label>{field.label}</label>
+				<input
+					className="form-control"
+					type="text"
+					{...field.input}
+				/>
+			</div>
+		)
+	}
 	render() {
 		return(
 			<form>
 				<Field
+					label="Title"
 					name="title"
-					component={}
+					component={this.renderField}
+				/>
+				<Field
+					label="Categories"
+					name="categories"
+					component={this.renderField}
+				/>
+				<Field
+					label="Post Content"
+					name="content"
+					component={this.renderField}
 				/>
 			</form>
 		)
 	}
 }
 
+function validate(values) {
+	const errors = {};
+	
+	if (!values.title) {
+		errors.title = "Enter a title!";
+	}
+	if (!values.categories) {
+		errors.categories = "Enter a title!";
+	}
+	if (!values.content) {
+		errors.content = "Enter some content!";
+	}
+	// if errors is empty, the form is fine to submit
+	// if errors has any properties, redux form assumes form is invalid
+	return errors;
+}
+
 export default reduxForm({
+	validate: validate,
 	form: 'PostsNewForm'
 })(PostsNew);
